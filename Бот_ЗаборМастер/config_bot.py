daf1c3db8ce9635e652233fc07391c72df32bc46
Aynur-Safina <aynur.safina2020@gmail.com>
2023-11-16 11:56:27 +0300

    delete config file

--- Бот_ЗаборМастер/config_bot.py
+++
@@ -1,8 +0,0 @@
-TOKEN = '6986278407:AAEJHNgSxEoIp8h8IE0WLn61KoeQuTenqX8'
-NAME = 'ЗаборМастер'
-NIK = 'https://t.me/Zabor_Kzn_bot'
-
-#User id: 6986278407
-#chat.id:-4078632692
-
-#https://api.telegram.org/bot6986278407:AAEJHNgSxEoIp8h8IE0WLn61KoeQuTenqX8/getUpdates
